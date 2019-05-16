381b6ce6738e8a31f4af24f6ea48f38340fa8b8f
Ralph Eclipse <Ralph.Eclipse@Quantrics.Local>
2019-05-16 21:58:15 +0800

    UI Updates

--- App.svelte
+++ App.svelte
@@ -1,5 +1,9 @@
 <script>
   import { getFieldsFromTemplate, setClipboard, sleep } from "./utils";
+  let notifications = [];
+  const notificationTimeout = 5000;
+  const notificationCopyMessage =
+    "Description copied to clipboard. You can now paste it into Siebel";
   let currentTemplate = `name:
 password:
   `;
@@ -22,21 +26,91 @@ password:
 
   const handleClick = async () => {
     await setClipboard(result);
+    const notification = {
+      id: +new Date(),
+      message: notificationCopyMessage
+    };
+    notifications = [notification, ...notifications];
+    await sleep(notificationTimeout);
+    notifications = notifications.filter(n => n.id !== notification.id);
   };
 </script>
 
 <style>
+  :global(*) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Helvetica, Arial, sans-serif;
+  }
   main {
     font-family: sans-serif;
     text-align: center;
+    margin: 20px;
   }
   input {
+    width: 100%;
     display: block;
     border: 1px solid #000;
-    width: 100%;
     padding: 10px 5px;
-    margin-bottom: 10px;
+    margin: 10px auto 10px auto;
     outline: 0;
+    transition: 0.3s ease-in-out;
+  }
+  input::placeholder {
+    text-transform: uppercase;
+  }
+  input:focus {
+    background: #222;
+    color: white;
+  }
+  label {
+    font-size: 20px;
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+  button {
+    border: 1px solid #000;
+    outline: 0;
+    padding: 20px 10px;
+    font-weight: bold;
+    font-size: 20px;
+    cursor: pointer;
+    background: #fff;
+    transition: 0.3s ease-in-out;
+  }
+  button:hover {
+    color: #fff;
+    background: #000;
+  }
+  pre {
+    border: 1px solid #000;
+    padding: 20px;
+    margin: 20px auto;
+    text-align: left;
+  }
+  textarea {
+    width: 100%;
+    height: calc(100vh - 500px);
+  }
+  .notifications {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    pointer-events: none;
+  }
+  .notification {
+    padding: 10px;
+    background: #000;
+    color: white;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 40%;
   }
 </style>
 
@@ -52,4 +126,9 @@ password:
   <pre>{result}</pre>
   <h3>Put your template here</h3>
   <textarea bind:value={currentTemplate} rows="4" cols="100" />
+  <div class="notifications">
+    {#each notifications as notification (notification.id)}
+      <div class="notification"> {notification.message} </div>
+    {/each}
+  </div>
 </main>
